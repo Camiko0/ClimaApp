d8a7c924fa7ceea20414a121cc1adb0d54ec24ad
LizetPatino <lizeth0330@outlook.com>
2019-01-24 22:29:42 -0500

    estilos forecastExtended y planeacion fetch

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import './styles.css';
 import ForecastItem from './ForecastItem';
 
+/*const days =["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+const data={
+    temperature: 10,
+    humidity: 5,
+    weatherState: 'normal',
+    wind: 'normal',
+}; */
+
 class ForecastExtended extends Component {
     
+    constructor(){
+        super();
+        this.state={
+            forecastData:null,
+        };
+    }
+    
+    renderForecastItemDays() {
+        return "render items";
+      //  days.map(day => (<ForecastItem key={day} weekDay={day} hour={10} data={data} />))
+    }
+    
+    renderProgress = () => {
+        return <h3>Cargando Pronostico Extendido...</h3>;
+    }
+
     render(){
         const {city} = this.props;
+        const {forecastData}=this.state;
         return(
             <div>
                <h2 className="forecast-title"> Pronóstico Extendido para {city} </h2>
-               <ForecastItem/>
+                {forecastData ? 
+                this.renderForecastItemDays():
+                this.renderProgress()}
             </div>
         );
     }
--- src/components/ForecastItem/index.js
+++ src/components/ForecastItem/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import WeatherData from '../WeatherLocation/WeatherData';
 
-const ForecastItem = () =>(
-    <div>Item</div>
+
+const ForecastItem = ({weekDay,hour,data}) =>(
+    <div>
+        <div>{weekDay} Hora: {hour} hs </div>
+        <WeatherData data={data}/>
+    </div>
+    
+    
 );
 
 export default ForecastItem;
