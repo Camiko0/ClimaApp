8f0f14fa8629c24b13352e0da8fd66af32d6c732
LizetPatino <lizeth0330@outlook.com>
2019-01-26 19:41:58 -0500

    Updates

---
+++ precache-manifest.4e0a36c1793babf5dcb39f0b48b224d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26c77a0a3e8ae99a2224",
+    "url": "/ClimaApp/static/js/runtime~main.26c77a0a.js"
+  },
+  {
+    "revision": "2d86caac7e1161bd3cdc",
+    "url": "/ClimaApp/static/js/main.2d86caac.chunk.js"
+  },
+  {
+    "revision": "4915ff5d4093fa24ca5f",
+    "url": "/ClimaApp/static/js/1.4915ff5d.chunk.js"
+  },
+  {
+    "revision": "2d86caac7e1161bd3cdc",
+    "url": "/ClimaApp/static/css/main.496a4416.chunk.css"
+  },
+  {
+    "revision": "4915ff5d4093fa24ca5f",
+    "url": "/ClimaApp/static/css/1.541d2420.chunk.css"
+  },
+  {
+    "revision": "3bef00e9ae90fa3795adef0152cc330b",
+    "url": "/ClimaApp/index.html"
+  }
+];
